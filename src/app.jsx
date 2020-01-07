e49e61e96abe74324316f5b5174b16f8f54caf10
Simon Kobyda <skobyda@redhat.com>
2020-01-07 16:50:41 +0100

    Init project from cockpit starter-kit

--- src/app.jsx
+++ src/app.jsx
@@ -36,9 +36,9 @@ export class Application extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <h2>Starter Kit</h2>
+                <h2>Certificates</h2>
                 <p>
-                    { cockpit.format(_("Running on $0"), this.state.hostname) }
+                    { cockpit.format(_("Running on $0"), path) }
                 </p>
             </div>
         );
