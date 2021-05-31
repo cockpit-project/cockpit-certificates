6d0b1ab54f7aa2b0866be7947b5f305795127c09
Katerina Koukiou <kkoukiou@redhat.com>
2021-05-31 09:14:43 +0200

    Modernize webpack.config
    * Move babel configuration in seperate file
    * Use Terser plugin for production builds
    * Add check for missing sassc loader
    * Use current babel/eslint integration (babel-eslint is now @babel/eslint-parser)

--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,50 @@
 const childProcess = require('child_process');
 const path = require("path");
+
 const copy = require("copy-webpack-plugin");
 const extract = require("mini-css-extract-plugin");
-const fs = require("fs");
-const webpack = require("webpack");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
 const CockpitPoPlugin = require("./src/lib/cockpit-po-plugin");
 
-var externals = {
-    "cockpit": "cockpit",
-};
+const webpack = require("webpack");
 
-/* These can be overridden, typically from the Makefile.am */
-const srcdir = (process.env.SRCDIR || __dirname) + path.sep + "src";
-const builddir = (process.env.SRCDIR || __dirname);
-const distdir = builddir + path.sep + "dist";
-const section = process.env.ONLYDIR || null;
 const nodedir = path.resolve((process.env.SRCDIR || __dirname), "node_modules");
 
 /* A standard nodejs and webpack pattern */
-var production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production';
 
-var info = {
-    entries: {
-        "index": [
-            "./index.js"
-        ]
-    },
-    files: [
-        "index.html",
-        "manifest.json",
-    ],
-};
-
-var output = {
-    path: distdir,
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-};
-
-/*
- * Note that we're avoiding the use of path.join as webpack and nodejs
- * want relative paths that start with ./ explicitly.
- *
- * In addition we mimic the VPATH style functionality of GNU Makefile
- * where we first check builddir, and then srcdir.
- */
-
-function vpath(/* ... */) {
-    var filename = Array.prototype.join.call(arguments, path.sep);
-    var expanded = builddir + path.sep + filename;
-    if (fs.existsSync(expanded))
-        return expanded;
-    expanded = srcdir + path.sep + filename;
-    return expanded;
-}
-
-/* Qualify all the paths in entries */
-Object.keys(info.entries).forEach(function(key) {
-    if (section && key.indexOf(section) !== 0) {
-        delete info.entries[key];
-        return;
-    }
-
-    info.entries[key] = info.entries[key].map(function(value) {
-        if (value.indexOf("/") === -1)
-            return value;
-        else
-            return vpath(value);
-    });
-});
-
-/* Qualify all the paths in files listed */
-var files = [];
-info.files.forEach(function(value) {
-    if (!section || value.indexOf(section) === 0)
-        files.push({ from: vpath("src", value), to: value });
-});
-info.files = files;
-
-var plugins = [
-    new copy(info.files),
+// Non-JS files which are copied verbatim to dist/
+const copy_files = [
+    "./src/index.html",
+    "./src/manifest.json",
+];
+const plugins = [
+    new copy({ patterns: copy_files }),
     new extract({filename: "[name].css"}),
+    new ESLintPlugin({ extensions: ["js", "jsx"] }),
     new CockpitPoPlugin(),
 ];
 
 /* Only minimize when in production mode */
 if (production) {
-    /* Rename output files when minimizing */
-    output.filename = "[name].min.js";
-
     plugins.unshift(new CompressionPlugin({
-        asset: "[path].gz[query]",
-        test: /\.(js|html)$/,
-        minRatio: 0.9,
+        test: /\.(js|html|css)$/,
         deleteOriginalAssets: true
     }));
 }
 
-var babel_loader = {
-    loader: "babel-loader",
-    options: {
-        presets: [
-            ["@babel/env", {
-                "targets": {
-                    "chrome": "57",
-                    "firefox": "52",
-                    "safari": "10.3",
-                    "edge": "16",
-                    "opera": "44"
-                }
-            }],
-            "@babel/preset-react"
-        ]
+/* check if sassc is available, to avoid unintelligible error messages */
+try {
+    childProcess.execFileSync('sassc', ['--version'], { stdio: ['pipe', 'inherit', 'inherit'] });
+} catch (e) {
+    if (e.code === 'ENOENT') {
+        console.error("ERROR: You need to install the 'sassc' package to build this project.");
+        process.exit(1);
+    } else {
+        throw e;
     }
 }
 
@@ -130,7 +62,11 @@ try {
 
 module.exports = {
     mode: production ? 'production' : 'development',
-    entry: info.entries,
+    entry: {
+        index: [
+            "./src/index.js",
+        ]
+    },
     resolve: {
         modules: [ nodedir, path.resolve(__dirname, 'src/lib') ],
         alias: { 'font-awesome': path.resolve(nodedir, 'font-awesome-sass/assets/stylesheets') },
@@ -138,20 +74,27 @@ module.exports = {
     resolveLoader: {
         modules: [ nodedir, path.resolve(__dirname, 'src/lib') ],
     },
-    externals: externals,
-    output: output,
+    externals: { "cockpit": "cockpit" },
     devtool: "source-map",
+
+    optimization: {
+        minimize: production,
+        minimizer: [
+            new TerserJSPlugin({}),
+            // Workaround: https://github.com/patternfly/patternfly-react/issues/5650
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorPluginOptions: {
+                    preset: ['default', { mergeLonghand: false }]
+                }
+            })
+        ],
+    },
+
     module: {
         rules: [
             {
-                enforce: 'pre',
                 exclude: /node_modules/,
-                loader: 'eslint-loader',
-                test: /\.(js|jsx)$/
-            },
-            {
-                exclude: /node_modules/,
-                use: babel_loader,
+                use: "babel-loader",
                 test: /\.(js|jsx)$/
             },
             /* HACK: remove unwanted fonts from PatternFly's css */
